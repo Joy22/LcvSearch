fbc7dbaf8f1ecd3b290eba7ceb6e945d03557c7f
Joy22 <525993372@qq.com>
2017-11-10 16:41:04 +0800

    获取搜索结果，生成页面

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.http import HttpResponse
+from elasticsearch import Elasticsearch
 
 from search.models import ArticleType
 
@@ -8,6 +9,8 @@ import json
 import redis
 
 redis_cli = redis.StrictRedis()
+client = Elasticsearch(hosts=["127.0.0.1"])
+
 
 # Create your views here.
 class IndexView(View):
@@ -19,14 +22,69 @@ class IndexView(View):
 
 class SearchView(View):
     def get(self, request):
-        key_word = request.GET.get("q", "")
+        keyword = request.GET.get("q", "")
+        page = request.GET.get("p", "1")
+        try:
+            page = int(page)
+        except:
+            page = 1
 
         # redis关键词加1
-        redis_cli.zincrby("search_keywords_set", key_word)
+        redis_cli.zincrby("search_keywords_set", keyword)
 
         hot_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
 
-        return render(request, "result.html", {"hot_search": hot_search})
+        response = self.__get_search_response(keyword, page)
+        hit_list, total_nums = self.__parse_search_response(response)
+
+        return render(request, "result.html",
+                      {"hot_search": hot_search, "all_hits": hit_list, "total_nums": total_nums})
+
+    @staticmethod
+    def __get_search_response(keyword, page):
+        response = client.search(
+            index="jobbole",
+            body={
+                "query": {
+                    "multi_match": {
+                        "query": keyword,
+                        "field": ["tags", "title", "content"]
+                    }
+                },
+                "from": (page - 1) * 10,
+                "size": 10,
+                "highlight": {
+                    "pre_tags": ['<span class="keyWord>'],
+                    "post_tags": ['</span>'],
+                    "fields": {
+                        "title": {},
+                        "content": {}
+                    }
+                }
+            }
+        )
+        return response
+
+    @staticmethod
+    def __parse_search_response(response):
+        hit_list = []
+        for hit in response["hits"]["hits"]:
+            hit_dict = {}
+            if "title" in hit["highlight"]:
+                hit_dict["title"] = "".join(hit["highlight"]["title"])
+            else:
+                hit_dict["title"] = hit["_source"]["title"]
+            if "content" in hit["highlight"]:
+                hit_dict["content"] = "".join(hit["highlight"]["content"])[:500]
+            else:
+                hit_dict["content"] = hit["_source"]["content"][:500]
+            hit_dict["create_date"] = hit["_source"]["create_date"]
+            hit_dict["url"] = hit["_source"]["url"]
+            hit_dict["score"] = hit["_source"]
+            hit_list.append(hit_dict)
+
+        total_nums = response["hits"]["total"]
+        return hit_list, total_nums
 
 
 class SearchSuggestView(View):
@@ -46,5 +104,4 @@ class SearchSuggestView(View):
             for match in suggestions.article_suggest[0].options:
                 source = match._source
                 return_data.append(source["title"])
-        return  HttpResponse(json.dumps(return_data), content_type="application/json")
-
+        return HttpResponse(json.dumps(return_data), content_type="application/json")
